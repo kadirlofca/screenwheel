571a96da7fc13266e877c1b1423775c4995495b6
Kadir Lofca <kadirlofca@outlook.com>
2025-03-15 18:27:29 -0400

    Basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), svelte()],
+    vite: {
+        plugins: [tailwindcss()],
+      },
 });
---
+++ src/components/reactive/WheelMenu.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    const navigationLinks: string[] = [
+        "Home",
+        "About Me",
+        "Experience",
+        "Contact",
+    ]
+
+    function getIndexDistanceToSelectedNavigationLink(navigationLink: string): number {
+        
+
+        return 0;
+    }
+</script>
+
+<div class="flex flex-col justify-between p-8 text-right text-primary-foreground bg-primary">
+    <h1 class="text-xl">Name Lastname</h1>
+    <div class="flex flex-col gap-4 text-4xl">
+        {#each navigationLinks as navigationLink}
+            <span>{navigationLink}</span>
+        {/each}
+    </div>
+    <p class="text-sm opacity-60">Site design & development by Kadir Lofca.</p>
+</div>
