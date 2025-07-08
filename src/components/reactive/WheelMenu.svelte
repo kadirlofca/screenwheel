3929acb1e145f5a9c7647448655306d5b02f2d74
Kadir Lofca <kadirlofca@outlook.coM>
2025-07-08 13:53:34 -0400

    user created pages show up in wheel menu

--- src/components/reactive/WheelMenu.svelte
+++ src/components/reactive/WheelMenu.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-    let { items } = $props();
-
-    const navigationLinks: string[] = items;
+    import type { CollectionEntry } from "astro:content";
+    export let items: CollectionEntry<'sections'>[];
 
     function getIndexDistanceToSelectedNavigationLink(navigationLink: string): number {
-        
         return 0;
     }
 </script>
@@ -12,8 +10,8 @@
 <div class="flex flex-col justify-between p-8 text-right text-primary-foreground bg-primary">
     <h1 class="text-xl">Name Lastname</h1>
     <div class="flex flex-col gap-4 text-4xl">
-        {#each navigationLinks as navigationLink}
-            <span>{navigationLink}</span>
+        {#each items as item}
+            <span>{item.data.title}</span>
         {/each}
     </div>
     <p class="text-sm opacity-60">Site design & development by Kadir Lofca.</p>
